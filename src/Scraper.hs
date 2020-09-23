748909f27eea33c1f60ffe4633c0e30651795320
Gleb Dianov <uti55531@gmail.com>
2020-09-23 02:18:25 +0100

    use user timeline request instead of search

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -4,57 +4,66 @@ module Scraper
   where
 
 import Scraper.EmailFinder
-import Scraper.MaxId
 import TwitterAuth
 
 import Protolude
 
-import qualified Web.Twitter.Conduit as Twitter
-import qualified Web.Twitter.Types   as Twitter
+import qualified Web.Twitter.Conduit                  as Twitter
+import qualified Web.Twitter.Conduit.Request.Internal as Twitter
+import qualified Web.Twitter.Conduit.Status           as Twitter
+import qualified Web.Twitter.Types                    as Twitter
 
 
 scrapeRapperEmails :: Session -> IO ()
 scrapeRapperEmails session@Session{..} = do
-  putStrLn @Text "Starting search #1"
+  putStrLn @Text "Starting initial request"
 
-  result@Twitter.SearchResult{..} <-
+  statuses <-
     Twitter.call twInfo manager searchQuery
 
-  extractEmails $ Twitter.searchResultStatuses result
+  extractEmails statuses -- $ Twitter.searchResultStatuses result
 
+  case lastMay $ sortOn Twitter.statusId statuses of
+    Just Twitter.Status{..} ->
+      scrapeNext session 2 (statusId - 1)
+
+    Nothing ->
+      putStrLn @Text "headMay returned Nothing after first request"
+
+
+scrapeNext :: Session -> Int -> Twitter.StatusId -> IO ()
+scrapeNext session@Session{..} count maxId = do
   let Twitter.APIRequest{..} = searchQuery
 
-  scrapeNext session 2 searchResultSearchMetadata
+      nextQuery = searchQuery
+        { Twitter._params = ("max_id", Twitter.PVInteger maxId) : _params }
+
+  putStrLn @Text
+    $ "Starting search #" <> show count <> " with max_id=" <> show maxId
+
+  statuses <- Twitter.call twInfo manager nextQuery
+
+  extractEmails statuses
+
+  putStrLn @Text "Search complete. Emails extracted.\n"
+
+  case lastMay $ sortOn Twitter.statusId statuses of
+    Just Twitter.Status{..} ->
+      scrapeNext session (count + 1) (statusId - 1)
+
+    Nothing ->
+      putStrLn @Text "headMay returned Nothing"
 
 
-scrapeNext :: Session -> Integer -> Twitter.SearchMetadata -> IO ()
-scrapeNext session@Session{..} scrapeNum Twitter.SearchMetadata{..}
-  = case searchMetadataNextResults of
-      Nothing ->
-        putStrLn @Text "Ran out of tweets"
-
-      Just nextResults -> case extractMaxId nextResults of
-        Nothing ->
-          putStrLn @Text
-            $  "Parameter 'max_id' not found in the search metadata:\n"
-            <> show searchMetadataNextResults
-
-        Just maxId -> do
-          let Twitter.APIRequest{..} = searchQuery
-
-              nextQuery
-                = searchQuery { Twitter._params = ("max_id", maxId) : _params }
-
-          putStrLn @Text $ "Starting search #" <> show scrapeNum
-
-          result@Twitter.SearchResult{..} <-
-            Twitter.call twInfo manager nextQuery
-
-          extractEmails $ Twitter.searchResultStatuses result
-
-          scrapeNext session (scrapeNum + 1) searchResultSearchMetadata
-
-
-searchQuery :: Twitter.APIRequest Twitter.SearchTweets (Twitter.SearchResult [Twitter.Status])
+searchQuery :: Twitter.APIRequest Twitter.StatusesUserTimeline [Twitter.Status]
 searchQuery
-  = Twitter.searchTweets "(from:SendBeatsBot) -filter:links -filter:replies"
+  = let query = Twitter.userTimeline $ Twitter.ScreenNameParam botHandle
+
+    in query { Twitter._params
+                 = ("count", Twitter.PVInteger 2)
+                 : ("max_id", Twitter.PVInteger 1294726112400965631)
+                 : Twitter._params query
+             }
+
+  where
+    botHandle = "SendBeatsBot"
